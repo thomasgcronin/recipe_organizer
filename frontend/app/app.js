fdf2a491984e3c0eb276df040b1463f8b6049b1b
Thomas Cronin <thomasgcronin@gmail.com>
2015-04-08 20:43:50 -0600

    changed baseurl to /api for the server

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -11,6 +11,6 @@ angular.module('myApp', [
     .config(['$routeProvider', 'RestangularProvider', function ($routeProvider, RestangularProvider) {
         $routeProvider.otherwise({redirectTo: '/recipes' + ''});
 
-        RestangularProvider.setBaseUrl('http://localhost:8001')
+        RestangularProvider.setBaseUrl('/api')
 
     }]);
