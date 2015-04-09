05ddbf680bc678e9a84d023cf7ca09be40289a90
Thomas Cronin <thomasgcronin@gmail.com>
2015-04-08 21:12:34 -0600

    added slashes to end of urls.py endpoints, added setRequestSuffix to all Restangular calls so they reach out to endpoints with slashes at the end

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -11,6 +11,7 @@ angular.module('myApp', [
     .config(['$routeProvider', 'RestangularProvider', function ($routeProvider, RestangularProvider) {
         $routeProvider.otherwise({redirectTo: '/recipes' + ''});
 
-        RestangularProvider.setBaseUrl('http://localhost:8001')
+        RestangularProvider.setBaseUrl('http://localhost:8001');
+        RestangularProvider.setRequestSuffix('/');
 
     }]);
