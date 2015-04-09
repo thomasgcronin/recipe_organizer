c07281ba88317498a6921f78f9b9b53df9be4026
Thomas Cronin <thomasgcronin@gmail.com>
2015-04-08 21:34:50 -0600

    print statement to check MEDIA_ROOT

--- backend/recipe_organizer/settings.py
+++ backend/recipe_organizer/settings.py
@@ -59,6 +59,7 @@ WSGI_APPLICATION = 'recipe_organizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 MEDIA_ROOT = BASE_DIR + '/apps/recipes/media/photos'
+print MEDIA_ROOT
 
 MEDIA_URL = '/media/'
 
